0e00632a0c7dd5dbbdc453fc0e9eae3a256b3a70
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2024-01-29 16:09:43 +0400

    feat: add the index page

---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const NAV_LINKS = [
+  { href: '/kanban', text: 'Kanban' },
+  { href: '/chart1', text: 'Chart 1' },
+  { href: '/chart2', text: 'Chart 2' },
+];
+</script>
+
+<template>
+  <section>
+    <h1 class="sr-only">The links to kanban board and charts</h1>
+    <ul class="flex items-center gap-4">
+      <li v-for="link in NAV_LINKS" :key="link.href">
+        <NuxtLink
+          :to="link.href"
+          :class="'text-blue-500 hover:underline text-base sm:text-lg md:text-xl lg:text-2xl'"
+        >
+          {{ link.text }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </section>
+</template>
