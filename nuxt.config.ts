809fe77580ff48143c8c1157b382e8fff3297bd1
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2024-01-29 15:37:46 +0400

    build: init the app

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path';
+import { defineNuxtConfig } from 'nuxt/config';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    ['@nuxtjs/eslint-module', { lintOnStart: false }],
+  ],
+  alias: {
+    '@': resolve(__dirname, './src'),
+  },
+  imports: {
+    autoImport: false,
+  },
+});
