f9147c445bda57225cad0394281cd94fee84f0ba
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2024-01-30 10:01:57 +0400

    fix: fix routes and styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
 const NAV_LINKS = [
-  { href: '/kanban', text: 'Kanban' },
-  { href: '/chart1', text: 'Chart 1' },
-  { href: '/chart2', text: 'Chart 2' },
+  { href: '/kanban', text: 'The Kanban board' },
+  { href: '/histogram', text: 'The histogram' },
+  { href: '/dot-graph', text: 'The dot graph' },
 ];
 </script>
 
 <template>
   <section>
     <h1 class="sr-only">The links to kanban board and charts</h1>
-    <ul class="flex items-center gap-4">
+    <ul class="flex flex-col items-center gap-y-4">
       <li v-for="link in NAV_LINKS" :key="link.href">
-        <NuxtLink
-          :to="link.href"
-          :class="'text-blue-500 hover:underline text-base sm:text-lg md:text-xl lg:text-2xl'"
-        >
+        <NuxtLink :to="link.href" class="text-xl text-blue-500 hover:underline lg:text-2xl">
           {{ link.text }}
         </NuxtLink>
       </li>
