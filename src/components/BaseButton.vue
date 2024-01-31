91984c9bcdbfa729ff8b58ec466c5980d54b4006
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2024-01-31 10:15:11 +0400

    feat: add common components

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :type="type" class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { type ButtonHTMLAttributes, withDefaults } from 'vue';
+
+/** Типы пропсов */
+interface Props {
+  /** Тип кнопки */
+  type?: ButtonHTMLAttributes['type'];
+}
+
+/** Пропсы со значениями по умолчанию */
+withDefaults(defineProps<Props>(), {
+  type: 'button',
+});
+</script>
+
+<style scoped>
+.button {
+  @apply box-border inline-flex cursor-pointer items-center border-2 border-transparent text-center;
+}
+</style>
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as BaseButton } from './BaseButton.vue';
+export { default as CrossIcon } from './CrossIcon.vue';
+export { default as PencilIcon } from './PencilIcon.vue';
